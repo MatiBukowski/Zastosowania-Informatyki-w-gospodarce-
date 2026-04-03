17f2a8741e5a8679c5723c4cde3dbdb8870eb22e
Bircheses <kbrzoza03@outlook.com>
2026-04-03 20:07:03 +0200

    Add scroable restaurant list

--- frontend/mobile/src/app/index.tsx
+++ frontend/mobile/src/app/index.tsx
@@ -2,45 +2,13 @@ import React, { useEffect, useState } from 'react';
 import { Text, View, ActivityIndicator, SafeAreaView } from 'react-native';
 import { getRestaurants } from '../api/RestaurantAPI';
 import { theme } from '../theme/theme';
+import RestuarantView from '../views/RestaurantView';
 
 export default function HomePageMobile() {
-  const [status, setStatus] = useState<string>('Waiting for database connection...');
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    // testing connection
-    getRestaurants()
-      .then(data => {
-        // show the nr of restaurants to check connection
-        setStatus(`Working API! Restaurants: ${data?.length || 0}`);
-        setLoading(false);
-      })
-      .catch(err => {
-        setStatus(`API Error: ${err.message}`);
-        setLoading(false);
-      });
-  }, []);
-
   return (
     <SafeAreaView style={theme.common.screenContainer}>
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-
-        <Text style={theme.typography.h4}>
-          Restaurant App 🍴
-        </Text>
-
-        {loading ? (
-          <ActivityIndicator
-            size="large"
-            color={theme.colors.primary}
-            style={{ marginTop: 20 }}
-          />
-        ) : (
-          <Text style={[theme.typography.body, { marginTop: 20, color: theme.colors.text }]}>
-            {status}
-          </Text>
-        )}
-
+        <RestuarantView />
       </View>
     </SafeAreaView>
   );
