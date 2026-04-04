bc8a4a11f61f9077152d9cd7d8ff0a846d27c7ad
Dawid Jabłoński <xederro@gmail.com>
2026-04-04 15:31:41 +0200

    Add photo URLs to seed data and update Android CI workflow configuration

--- backend/src/services/seed.py
+++ backend/src/services/seed.py
@@ -7,11 +7,12 @@ import random
 fake = Faker('pl_PL')
 
 RESTAURANTS_DATA = [
-    {"name": "Abecadło z pieca spadło,O ziemię się hukło,Rozsypało się po kątach,Strasznie się potłukło:", "address": "ul. Włoska 1, Kraków", "cuisine": CuisineTypeEnum.ITALIAN, "has_kiosk": True, "description": "Authentic Italian cuisine with fresh ingredients."},
+    {"name": "Abecadło z pieca spadło,O ziemię się hukło,Rozsypało się po kątach,Strasznie się potłukło:", "address": "ul. Włoska 1, Kraków", "cuisine": CuisineTypeEnum.ITALIAN, "has_kiosk": True, "description": "Authentic Italian cuisine with fresh ingredients.", "photo": "https://minio.xederro.tech/prod/restaurant/0.jpg"},
 ]
 
 def generate_fake_restaurant() -> dict:
     cuisine = random.choice(list(CuisineTypeEnum))
+    photo_id = random.randint(0, 64)
 
     prefixes = {
         CuisineTypeEnum.ITALIAN: ["Trattoria", "Osteria", "Ristorante", "Pizzeria"],
@@ -35,6 +36,7 @@ def generate_fake_restaurant() -> dict:
         "cuisine": cuisine,
         "has_kiosk": random.choice([True, False]),
         "description": fake.sentence(nb_words=10),
+        "photo": f"https://minio.xederro.tech/prod/restaurant/{photo_id}.jpg",
         "is_active": True
     }
 
