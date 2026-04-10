4720d19157a74355d4c0143889fe4614bd3fbc1b
Bircheses <kbrzoza03@outlook.com>
2026-04-10 23:08:13 +0200

    Add refresh token generation

--- backend/src/security/token_provider.py
+++ backend/src/security/token_provider.py
@@ -4,23 +4,34 @@ from datetime import datetime, timedelta
 
 from models.app_user import AppUser
 
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-REFRESH_TOKEN_EXPIRE_DAYS = 30
-
 
 class TokenProvider:
     def __init__(self):
         self.secret_key = self._generate_secret_key()
-    
+        self.ACCESS_TOKEN_EXPIRE_MINUTES = 30
+        self.REFRESH_TOKEN_EXPIRE_DAYS = 30
+
+
     @staticmethod
     def _generate_secret_key() -> str:
         return secrets.token_urlsafe(32)
+    
     
     def generate_access_token(self, user: AppUser) -> str:
         payload = {
             "user_id": user.user_id,
             "email": user.email,
             "role": user.role.value,
-            "exp": datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+            "exp": datetime.utcnow() + timedelta(minutes=self.ACCESS_TOKEN_EXPIRE_MINUTES)
+        }
+        return jwt.encode(payload, self.secret_key, algorithm="HS256")
+    
+    
+    def generate_refresh_token(self, user: AppUser) -> str:
+        payload = {
+            "user_id": user.user_id,
+            "email": user.email,
+            "role": user.role.value,
+            "exp": datetime.utcnow() + timedelta(days=self.REFRESH_TOKEN_EXPIRE_DAYS)
         }
         return jwt.encode(payload, self.secret_key, algorithm="HS256")
