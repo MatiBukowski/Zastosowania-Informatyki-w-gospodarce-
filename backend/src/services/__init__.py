2845d019f799b4ad2f849ba93fc836328dbd38a9
MatiBukowski <mati.bukowski@icloud.com>
2026-04-11 21:39:13 +0200

    Add seed users service

--- backend/src/services/__init__.py
+++ backend/src/services/__init__.py
@@ -1,5 +1,6 @@
 from .seed.run_seeds import run_seed
 from .seed.seed_restaurants import seed_restaurants
 from .seed.seed_menu_items import seed_menu_items
+from .seed.seed_users import seed_users
 from .restaurant import RestaurantService
 from .menu import MenuService
