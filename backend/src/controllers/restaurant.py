1217d95529fc043b2a2e1596e81d0799ded2c406
Maciek Kusz <mcdarimak@gmail.com>
2026-04-13 23:47:20 +0200

    Table module integration after review

--- backend/src/controllers/restaurant.py
+++ backend/src/controllers/restaurant.py
@@ -3,10 +3,11 @@ from ..schemas import (
     RestaurantPublicResponse,
     SingleRestaurantPublicResponse,
     MenuItemResponse,
-    RestaurantTableResponse,
-    ReservationPublicResponse
+    TableResponse,
+    TableCreate,
+    TableUpdate
 )
-from ..services import RestaurantService, MenuService, RestaurantTableService, ReservationService
+from ..services import RestaurantService, MenuService, TableService
 
 
 router = APIRouter(prefix="/restaurants", tags=["Restaurants"])
@@ -22,37 +23,27 @@ def get_restaurants_endpoint(service: RestaurantService = Depends()):
     return service.get_restaurants()
 
 @router.get(
-        "/{restaurant_id}",
-        summary="Get specific restaurant details",
-        description="Get information about specific restaurant",
-        response_model=SingleRestaurantPublicResponse
-        )
+    "/{restaurant_id}",
+    summary="Get specific restaurant details",
+    description="Get information about specific restaurant",
+    response_model=SingleRestaurantPublicResponse
+)
 def get_restaurant_endpoint(restaurant_id: int, service: RestaurantService = Depends()):
     return service.get_restaurant(restaurant_id)
 
 @router.get(
-        "/{restaurant_id}/menu",
-        summary="Get specific restaurant menu",
-        description="Retrieve a list of all available menu ites for specific restaurant",
-        response_model=list[MenuItemResponse]
-        )
+    "/{restaurant_id}/menu",
+    summary="Get specific restaurant menu",
+    description="Retrieve a list of all available menu ites for specific restaurant",
+    response_model=list[MenuItemResponse]
+)
 def get_menu_endpoint(restaurant_id: int, service: MenuService = Depends()):
     return service.get_menu_for_restaurant(restaurant_id)
 
-@router.get(
-        "/{restaurant_id}/tables",
-        summary="Get specific restaurant tables",
-        description="Retrieve a list of all available tables for specific restaurant",
-        response_model=list[RestaurantTableResponse]
-        )
-def get_restaurant_tables_endpoint(restaurant_id: int, service: RestaurantTableService = Depends()):
-    return service.get_tables_for_restaurant(restaurant_id)
-
-@router.get(
-        "/{restaurant_id}/tables/{table_id}/reservations",
-        summary="Get specific table reservations",
-        description="Retrieve a list of all reservations for specific table",
-        response_model=list[ReservationPublicResponse]
-        )
-def get_table_reservations_endpoint(restaurant_id: int, table_id: int, service: ReservationService = Depends()):
-    return service.get_reservations_for_table(table_id)
+@router.post(
+    "/{restaurant_id}/tables",
+    summary="Create new table",
+    response_model=TableResponse
+)
+def post_table_endpoint(restaurant_id: int, table_data: TableCreate, service: TableService = Depends()):
+    return service.create_new_table(restaurant_id, table_data)
---
+++ backend/src/controllers/table.py
@@ -0,0 +1,49 @@
+from fastapi import APIRouter, Depends
+from uuid import UUID
+from ..schemas import TableResponse, TableUpdate, ReservationPublicResponse
+from ..services import TableService, ReservationService
+
+
+router = APIRouter(prefix="/tables", tags=["Tables - Public QR Scan"])
+
+
+@router.get(
+    "/resolve/{token}",
+    response_model=TableResponse,
+    summary="Resolve QR code",
+    description="Check if the QR code for the table exists"
+)
+def resolve_qr_token(token: UUID, service: TableService = Depends()):
+    return service.resolve_table_by_token(token)
+
+@router.patch(
+    "/{table_id}",
+    summary="Update existing table",
+    response_model=TableResponse
+)
+def update_table_endpoint(table_id: int, table_data: TableUpdate, service: TableService = Depends()):
+    return service.update_existing_table(table_id, table_data)
+
+@router.patch(
+    "/{table_id}/regenerate-qr-code",
+    summary="Regenerate QR code for existing table",
+    response_model=TableResponse
+)
+def regenerate_qr_code_token(table_id: int, service: TableService = Depends()):
+    return service.regenerate_table_qr_code(table_id)
+
+@router.get(
+    "/restaurant/{restaurant_id}",
+    summary="Get specific restaurant tables",
+    response_model=list[TableResponse]
+)
+def get_restaurant_tables_endpoint(restaurant_id: int, service: TableService = Depends()):
+    return service.get_tables_for_restaurant(restaurant_id)
+
+@router.get(
+    "/{table_id}/reservation",
+    summary="Get specific table reservations",
+    response_model=list[ReservationPublicResponse]
+)
+def get_table_reservations_endpoint(table_id: int, service: ReservationService = Depends()):
+    return service.get_reservations_for_table(table_id)
