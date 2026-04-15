380da53b78e8b525c1fab452367f3e543e583cc7
Bircheses <kbrzoza03@outlook.com>
2026-04-15 20:12:25 +0200

    add scanview that will use user's camera to scan qr code only

--- frontend/mobile/src/components/buttons/ScanQrButton.tsx
+++ frontend/mobile/src/components/buttons/ScanQrButton.tsx
@@ -1,11 +1,14 @@
 import { MaterialIcons } from '@expo/vector-icons';
 import { Text, View } from 'react-native';
+import { useRouter } from 'expo-router';
 
 import StyledButton from './StyledButton';
 
 export default function ScanQrButton() {
+  const router = useRouter();
+
   return (
-    <StyledButton>
+    <StyledButton onPress={() => router.push('/scan')}>
       <View style={{ flexDirection: 'column', alignItems: 'center', gap: 5 }}>
         <MaterialIcons name="qr-code-scanner" size={40} color="white" />
         <Text style={{ color: 'white', marginLeft: 8 }}>Scan QR Code</Text>
---
+++ frontend/mobile/src/views/ScanView.tsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import { CameraView, Camera } from 'expo-camera';
+
+export default function ScanView() {
+  const [hasPermission, setHasPermission] = useState<boolean | null>(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    const getCameraPermissions = async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    };
+
+    getCameraPermissions();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }: { type: string; data: string }) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  }
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission...</Text>;
+  }
+
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+    <View>
+      <CameraView
+        onBarcodeScanned={scanned ? undefined : handleBarCodeScanned}
+        barcodeScannerSettings={{
+          barcodeTypes: ["qr"],
+        }}
+        style={StyleSheet.absoluteFill}
+      />
+      {scanned && <Text style={{ color: 'white', textAlign: 'center', marginTop: 20 }}>Tap to scan again</Text>}
+    </View>
+  );
+}
