0690bf4b1516aee8a5f6b0015e9e573fb764cf02
Bircheses <kbrzoza03@outlook.com>
2026-04-27 23:01:06 +0200

    Fix merge errors

--- frontend/mobile/src/app/_layout.tsx
+++ frontend/mobile/src/app/_layout.tsx
@@ -5,32 +5,8 @@ import {useEffect} from "react";
 import { posthogClient } from '@/analitics/analitics';
 
 export default function RootLayout() {
-<<<<<<< HEAD
   const pathname = usePathname();
   const params = useGlobalSearchParams();
-=======
-  return (
-    <Stack
-      screenOptions={{
-        // global header
-        headerStyle: {
-          backgroundColor: theme.colors.background,
-        },
-        headerShown: false,
-        headerTintColor: theme.colors.white,
-        headerTitleStyle: {
-          fontWeight: "800",
-          fontSize: 20,
-        },
-        // global background
-        contentStyle: {
-          backgroundColor: theme.colors.background,
-        },
-        headerShadowVisible: false,
-      }}
-    >
-    </Stack>
->>>>>>> 448bbd5eb86fba9d5e7a9855b5b99088de29b14b
 
   useEffect(() => {
     posthogClient.screen(pathname, params)
