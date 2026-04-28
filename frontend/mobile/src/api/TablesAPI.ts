f2ba46ca499aebc4649a5ebe1aa7772f77bdfd70
Dawid Jabłoński <xederro@gmail.com>
2026-04-28 09:12:22 +0200

    Path fix

--- frontend/mobile/src/api/TablesAPI.ts
+++ frontend/mobile/src/api/TablesAPI.ts
@@ -2,6 +2,6 @@ import { apiClient } from './API';
 import { ITable, ICreateTable } from '@/context/interfaces';
 
 export const resolveTableByToken = async (token: string): Promise<ITable> => {
-  const response = await apiClient.get<ITable>(`/api/tables/resolve?token=${token}`);
+  const response = await apiClient.get<ITable>(`/api/tables/resolve/${token}`);
   return response.data;
 };
