1f7e9a826afd79dfc1e0cbe8a04634e2e6ce1c7d
Dawid Jabłoński <xederro@gmail.com>
2026-05-05 23:34:42 +0200

    Add role-based access control for forecast endpoint
    - Implement `RoleChecker` for verifying user roles.
    - Add `get_current_user` method in `TokenProvider` to extract user information from JWT.
    - Restrict `/forecast` endpoint access to admins and managers using `RoleChecker`.

--- backend/src/controllers/forecast.py
+++ backend/src/controllers/forecast.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Depends
 
 from ..schemas import ForecastBase
 from ..services.forecast import ForecastService
+from ..security import RoleChecker
+from ..models import UserRoleEnum
 
 router = APIRouter(tags=["Forecast"])
 
@@ -9,7 +11,8 @@ router = APIRouter(tags=["Forecast"])
 @router.get(
     "/forecast/{restaurant_id}",
     summary="Forecast",
-    description=""
+    description="",
+    dependencies=[Depends(RoleChecker([UserRoleEnum.ADMIN, UserRoleEnum.MANAGER]))]
 )
 def get_forecast_endpoint(restaurant_id: int, service: ForecastService = Depends()) -> ForecastBase:
     return service.get_forecast(restaurant_id)
---
+++ backend/src/security/role_checker.py
@@ -0,0 +1,17 @@
+from fastapi import Depends, HTTPException, status
+
+from . import TokenProvider
+from src.models import UserRoleEnum, AppUser
+
+class RoleChecker:
+    def __init__(self, allowed_roles: list[UserRoleEnum]):
+        self.allowed_roles = allowed_roles
+
+    def __call__(self, token_provider: TokenProvider = Depends(TokenProvider)):
+        user: AppUser = token_provider.get_current_user()
+        if user.role not in self.allowed_roles:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="You do not have enough permissions"
+            )
+        return user
