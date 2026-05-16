833c9a6b680df03774790bef4439be285a8fff39
Dawid Jabłoński <xederro@gmail.com>
2026-05-16 15:46:52 +0200

    Fix ci by updating requirements

--- backend/src/repositories/restaurant.py
+++ backend/src/repositories/restaurant.py
@@ -26,11 +26,6 @@ class RestaurantRepository:
 
         if not user:
             return [], 0
-
-        # Note: SQLAlchemy collection doesn't easily support offset/limit at the property level
-        # Better to query Restaurant directly filtering by user_id if that relationship exists, 
-        # but let's see how it's defined. Assuming many-to-many or many-to-one.
-        # If user.managed_restaurants is a list, we can manually slice it or use a query.
         
         query = select(Restaurant).join(Restaurant.admins).where(AppUser.user_id == user_id)
         total = self.db.execute(select(func.count()).select_from(query.subquery())).scalar_one()
