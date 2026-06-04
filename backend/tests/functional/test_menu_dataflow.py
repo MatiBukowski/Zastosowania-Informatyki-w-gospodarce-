08d6f0e1a3daaf9477907e22a5e62752ee7c429c
MatiBukowski <mati.bukowski@icloud.com>
2026-06-04 17:29:03 +0200

    Add ftests for post menu item dataflow

--- backend/tests/functional/test_menu_dataflow.py
+++ backend/tests/functional/test_menu_dataflow.py
@@ -29,3 +29,34 @@ class TestMenuDataflow:
 
         data = response.json()
         assert data["items"] == []
+
+    def test_post_new_menu_item_flow_success(self, client, db):
+        create_menu(db)
+
+        payload = {
+            "name": "Burger",
+            "price": 35.55,
+            "description": "test description"
+        }
+
+        response = client.post("/api/restaurants/1/menu", json=payload)
+
+        assert response.status_code == 200 
+
+        data = response.json()
+        
+        assert "menu_item_id" in data
+        assert data["name"] == payload["name"]
+        assert float(data["price"]) == payload["price"]
+        assert data["description"] == payload["description"]
+
+    def test_post_new_menu_item_validation_error_flow(self, client, db):
+        create_menu(db)
+
+        bad_payload = {
+            "name": "name"
+        }
+
+        response = client.post("/api/restaurants/1/menu", json=bad_payload)
+
+        assert response.status_code == 422
